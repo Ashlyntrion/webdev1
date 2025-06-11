17a35857c6df02b6115a02eb6d9a6a4f8048dd4f
ashlyn <ashlynramesh@gmail.com>
2025-06-11 10:26:38 +0530

    Routing

--- frontend/src/components/pages/Login.js
+++ frontend/src/components/pages/Login.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React from "react";
+import ResponsiveAppBar from "../navbar/Navbar";
 
 export default function Login() {
-  return (
-    <div>
-        <h1>Login</h1>
-        <form>
-            <div>
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" required />
-            </div>
-            <div>
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" required />
-            </div>
-            <div>
-                <input type="submit" value="Login" />
-            </div>
-        </form>
-    </div>
-  )
+    return (
+        <div>
+            <ResponsiveAppBar />
+            <h1>Login</h1>
+            <form>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" required />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        required
+                    />
+                </div>
+                <div>
+                    <input type="submit" value="Login" />
+                </div>
+            </form>
+        </div>
+    );
 }
